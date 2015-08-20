e2c7384178e0fac178a5ea91a22a0a56cca910c5
Victor Hornets <vhornets@gmail.com>
2015-08-20 15:07:32 +0300

    Change slightly project structure \n -- Rename compiled css-file name \n --Replace form submit handler with event trigger \n --Add missing 'sprite-classes.less' file \n --Ad some mixins

--- js/app.js
+++ js/app.js
@@ -12,27 +12,7 @@ $('form').each(function() {
         ignore: "",
 
         submitHandler: function(form) {
-            var $form = $(form);
-            var $button = $form.find('input[type="submit"]');
-            var button_old_text = $button.val();
-            var data = $form.serialize() + '&action=sendrequest';
-
-            $button.val('Отправка...');
-
-            $.ajax('/wp-admin/admin-ajax.php', { method: 'post', data: data })
-            .done(function(res) {
-                var $response = $form.parent().find('.form-response');
-                var $response_text = $form.parent().find('.form-response__text');
-
-                $response_text.text(res);
-                $response.fadeIn('slow');
-                $button.val(button_old_text);
-
-                setTimeout(function() {
-                    $response.fadeOut('slow');
-                    $form[0].reset();
-                }, 3000);
-            });
+            $(form).trigger('form.valid');
         }
     });
 });
